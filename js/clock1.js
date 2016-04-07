f289068967a9b6355fc590d439467c487fd26625
Eric Lubbers <ericjlubbers@gmail.com>
2016-04-07 16:29:02 -0600

    Upload of nearly complete code. Next step: switch to moment.js.

---
+++ js/clock1.js
@@ -0,0 +1,22 @@
+tday=new Array("Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday");
+tmonth=new Array("January","February","March","April","May","June","July","August","September","October","November","December");
+
+function startTime() {
+  var d=new Date();
+  var nday=d.getDay(),nmonth=d.getMonth(),ndate=d.getDate(),nyear=d.getYear();
+  if(nyear<1000) nyear+=1900;
+  var nhour=d.getHours(),nmin=d.getMinutes(),nsec=d.getSeconds(),ap;
+  var today = new Date();
+  var h = today.getHours();
+  var m = today.getMinutes();
+  var s = today.getSeconds();
+  m = checkTime(m);
+  s = checkTime(s);
+  document.getElementById('txt').innerHTML =
+  "<h2 class=\"clock\">" + h + ":" + m + "<span class=\"seconds\">:" + s + "</span>" + "</h2>" + "<br />" + "<h2 class=\"day\">" + nday + ;
+  var t = setTimeout(startTime, 500);
+}
+function checkTime(i) {
+    if (i < 10) {i = "0" + i};  // add zero in front of numbers < 10
+    return i;
+  }
---
+++ js/date-time.js
@@ -0,0 +1,30 @@
+function date_time(id)
+{
+        date = new Date;
+        year = date.getFullYear();
+        month = date.getMonth();
+        months = new Array('January', 'February', 'March', 'April', 'May', 'June', 'Jully', 'August', 'September', 'October', 'November', 'December');
+        d = date.getDate();
+        day = date.getDay();
+        days = new Array('Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday');
+        h = date.getHours();
+        if(h<10)
+        {
+                h = "0"+h;
+        }
+        m = date.getMinutes();
+        if(m<10)
+        {
+                m = "0"+m;
+        }
+        s = date.getSeconds();
+        if(s<10)
+        {
+                s = "0"+s;
+        }
+        result = '<h2 class=\"clock\">' + h + ':' + m + '<span class=\"seconds\">: ' + s + '</h2><br /><h2 class=\"day\">' + days[day] + '</h2><br /><h2 class=\"date\">' + months[month]+ ' ' + d; 
+        // result = ''+days[day]+' '+months[month]+' '+d+' '+year+' '+h+':'+m+':'+s;
+        document.getElementById(id).innerHTML = result;
+        setTimeout('date_time("'+id+'");','1000');
+        return true;
+}
